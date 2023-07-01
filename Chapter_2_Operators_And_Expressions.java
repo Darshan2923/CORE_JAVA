f23e41f3f091b249b635de3cbca5cf1302caca53
Darshan <darshanpatel2923@gmail.com>
2023-07-01 11:31:28 +0530

    Added Chapter 2-Operators& Expressions

---
+++ Chapter_2_Operators_And_Expressions.java
@@ -0,0 +1,26 @@
+public class Chapter_2_Operators_And_Expressions {
+    public static void main(String[] args) {
+        // Types of operators
+        /*
+         * Arithmetic: +,-,*,/,%,++,--.
+         * Assignment: =,+=,-=.
+         * Comparison: ==,>,<,<=,>=.
+         * Logical: &&,||, !.
+         * Bitwise: &, | (Operates bitwise).
+         */
+
+        // Operators Precedence and associativity
+        /*
+         * https://www.researchgate.net/figure/Precedence-and-Associativity-of-Operators
+         * -in-Java-6_fig2_224187627
+         */
+
+        // Increment and decrement operators
+        /*
+         * a++ : first use then increment.
+         * ++a : first increment then use.
+         * a-- : first use then decrement.
+         * --a : first decrement then use.
+         */
+    }
+}
