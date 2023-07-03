ad18e64cb6ba3ca0655c08addd715cd10c807294
Darshan <darshanpatel2923@gmail.com>
2023-07-03 13:34:34 +0530

    Completed Programs on arrays Chapter05

--- Chapter_5_Problems_on_Arrays.java
+++ Chapter_5_Problems_on_Arrays.java
@@ -1,5 +1,58 @@
+// import java.util.Scanner;
+
 public class Chapter_5_Problems_on_Arrays {
     public static void main(String[] args) {
         System.out.println("This is the problem set for arrays in java");
+        // 1.Create an array of 5 floats and calculate their sum.
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * float[] nums = new float[5];
+         * for (int i = 0; i < nums.length; i++) {
+         * nums[i] = sc.nextFloat();
+         * }
+         * float sum = 0.0f;
+         * for (float f : nums) {
+         * sum += f;
+         * }
+         * System.out.println(sum);
+         */
+
+        // 2.Find average marks of all the students in a class
+        // Ans. Same as above, just divide by the total number of students.
+
+        // 3.WAP in java to reverse an array
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * int[] nums = new int[5];
+         * for (int i = 0; i < nums.length; i++) {
+         * nums[i] = sc.nextInt();
+         * }
+         * int[] temp;
+         * int start=0;
+         * int end=4;
+         * while(start<end){
+         * temp=nums[start];
+         * nums[start]=nums[end];
+         * nums[end]=temp;
+         * start++;
+         * end--;
+         * }
+         */
+
+        // 4.WAP to find maximum of an array
+        /*
+         * int max = 0;
+         * for (int i = 0; i < nums.length; i++) {
+         * if (nums[i] > max) {
+         * max = nums[i];
+         * }
+         * }
+         * System.out.println(max);
+         */
+
+        // 5.Find whether the array is sorted or not.
+        // Ans.Easy program. (Homework).
+
     }
+
 }
