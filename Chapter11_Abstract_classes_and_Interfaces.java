6b2f4334e93dd94e07761b4d7a3ec2d525b0802b
Darshan <darshanpatel2923@gmail.com>
2023-07-15 19:26:57 +0530

    Started with chapter11 Abstract classes &Interface

---
+++ Chapter11_Abstract_classes_and_Interfaces.java
@@ -0,0 +1,7 @@
+public class Chapter11_Abstract_classes_and_Interfaces {
+    public static void main(String[] args) {
+        // Abstract classes cannot be instantiated
+        // Rest will be done tommorrow
+    }
+
+}
