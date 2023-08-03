6415cac3a218b497dffc4aac0586a75e3f8ab744
Darshan <darshanpatel2923@gmail.com>
2023-08-03 11:17:38 +0530

    started with tutorial on packages in java

---
+++ Chapter_12_packages_in_java.java
@@ -0,0 +1,5 @@
+public class Chapter_12_packages_in_java {
+    public static void main(String[] args) {
+        System.out.println("Will start with this tutorial in packages in java later");
+    }
+}
