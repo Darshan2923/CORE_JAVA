7e955229530e74efe3f2e531e14c38a1c70e6114
Darshan <darshanpatel2923@gmail.com>
2023-08-11 19:50:02 +0530

    Started with advanced java 2

--- Advanced_java_2.java
+++ Advanced_java_2.java
@@ -1,5 +1,5 @@
 public class Advanced_java_2 {
     public static void main(String[] args) {
-        System.out.println("Advanced java 2");
+        System.out.println("Advanced java-2");
     }
 }
