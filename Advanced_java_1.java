aa1f75fb9ef58aebee7b3f847b57e5e5f6e5ebac
Darshan <darshanpatel2923@gmail.com>
2023-08-12 21:57:48 +0530

    Added a info related to module

--- Advanced_java_1.java
+++ Advanced_java_1.java
@@ -2,5 +2,8 @@ public class Advanced_java_1 {
     public static void main(String[] args) {
 
         System.out.println("Advanced java tut-1");
+        // In this module, we'll talk about collections in java and how to make javadocs
+        // in next segment..
+        // We'll also learn how to run java code without code editor
     }
 }
