9b7a14c6e11e542893b0c9f10532f95721c5540d
Darshan <darshanpatel2923@gmail.com>
2023-08-12 21:58:30 +0530

    Added info about the module

--- Advanced_java_2.java
+++ Advanced_java_2.java
@@ -1,5 +1,6 @@
 public class Advanced_java_2 {
     public static void main(String[] args) {
         System.out.println("Advanced java-2");
+        // In this segment we'll learn about javadocs
     }
 }
