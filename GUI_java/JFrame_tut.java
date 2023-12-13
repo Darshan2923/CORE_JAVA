5ddf57bada2e5c57d3b16fd10b4b527b062d9e19
Darshan <darshanpatel2923@gmail.com>
2023-12-13 22:11:44 +0530

    Made a form

--- GUI_java/JFrame_tut.java
+++ GUI_java/JFrame_tut.java
@@ -4,12 +4,17 @@ import java.awt.*;
 public class JFrame_tut {
     public static void main(String[] args) {
 
+        JLabel label = new JLabel();
+        label.setText("Enter your name");
+        label.setBackground(Color.white);
+
         JFrame frame = new JFrame();
         frame.setTitle("Our first jframe tut");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
         frame.setResizable(false);
         frame.setSize(450, 450);
-        frame.getContentPane().setBackground(new Color(0x123456));
+        frame.getContentPane().setBackground(new Color(0xFFFFFF));
+        frame.add(label);
     }
 }
